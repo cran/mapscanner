5f0d3580690ce83e44f3b8eed2d0a8e7bd26a491
Mark Padgham <mark.padgham@email.com>
2024-07-18 16:30:06 +0000

    version 0.1.1

--- R/file-manipulation.R
+++ R/file-manipulation.R
@@ -19,25 +19,25 @@
 #'
 #' @export
 ms_rotate_map <- function (map_original, map_modified, rotation = 0,
-                           apply_rotation = FALSE)
-{
+                           apply_rotation = FALSE) {
     map_original <- get_map_png (map_original, quiet = TRUE)
     map_modified <- get_map_png (map_modified, quiet = TRUE)
 
-    if (!apply_rotation)
+    if (!apply_rotation) {
         f <- file.path (tempdir (), "test.png")
-    else
+    } else {
         f <- map_modified
+    }
 
     magick::image_read (map_modified) %>%
         magick::image_rotate (rotation) %>%
         magick::image_write (f)
 
-    if (!apply_rotation)
-    {
+    if (!apply_rotation) {
         if (!requireNamespace ("mmand")) {
             stop ("rotation requires the 'mmand' package to be installed",
-                  call. = FALSE)
+                call. = FALSE
+            )
         }
         map <- png::readPNG (map_original)
         map_scanned <- png::readPNG (f)
@@ -55,8 +55,7 @@ ms_rotate_map <- function (map_original, map_modified, rotation = 0,
 }
 
 # params are names of .png files
-check_rotation <- function (map_original, map_modified)
-{
+check_rotation <- function (map_original, map_modified) {
     o <- magick::image_read (map_original) %>%
         magick::image_info ()
     o_w2h <- o$width / o$height
@@ -65,24 +64,25 @@ check_rotation <- function (map_original, map_modified)
     m_w2h <- m$width / m$height
 
     rotated <- FALSE
-    if (sign (1 - o_w2h) != sign (1 - m_w2h))
-        rotated <- TRUE # nocov
+    if (sign (1 - o_w2h) != sign (1 - m_w2h)) {
+        rotated <- TRUE
+    } # nocov
 
     return (rotated)
 }
 
 # get name of png file, converting pdf to png if neccesary
-get_map_png <- function (mapfile, quiet = TRUE)
-{
+get_map_png <- function (mapfile, quiet = TRUE) {
     png_name <- paste0 (tools::file_path_sans_ext (mapfile), ".png")
-    if (!(file.exists (mapfile) | file.exists (png_name)))
+    if (!(file.exists (mapfile) || file.exists (png_name))) {
         stop ("Neither ", mapfile, " nor ", png_name, " exist")
+    }
 
-    if (!file.exists (png_name))
-        pdf_to_png (mapfile) # nocov
+    if (!file.exists (png_name)) {
+        pdf_to_png (mapfile)
+    } # nocov
 
-    if (file.size (png_name) > 1e6)
-    {
+    if (file.size (png_name) > 1e6) {
         png_name <- reduce_size (png_name, quiet = quiet) # nocov
     }
     return (png_name)
@@ -90,11 +90,11 @@ get_map_png <- function (mapfile, quiet = TRUE)
 
 # nocov start
 # the following 2 functions are not currently tested
-pdf_to_png <- function (file)
-{
+pdf_to_png <- function (file) {
     file <- paste0 (tools::file_path_sans_ext (file), ".pdf")
-    if (!file.exists (file))
+    if (!file.exists (file)) {
         stop ("file ", file, " does not exist")
+    }
 
     bb <- bbox_from_pdf (file, as_string = TRUE)
 
@@ -104,21 +104,20 @@ pdf_to_png <- function (file)
     magick::image_write (img, path = fout, comment = bb)
 }
 
-hash <- function (len = 10)
-{
+hash <- function (len = 10) {
     sample (c (letters, LETTERS, 0:9), len, replace = TRUE) %>%
         paste0 (collapse = "")
 }
 
 # nocov start
-reduce_size <- function (mapfile, quiet = TRUE)
-{
+reduce_size <- function (mapfile, quiet = TRUE) {
     s <- file.size (mapfile)
-    if (!quiet)
-    {
+    if (!quiet) {
         smb <- formatC (s / 1e6, format = "f", digits = 1)
         message (cli::symbol$pointer, " Reducing size of '", mapfile,
-                 "' of ", smb, "MB", appendLF = FALSE)
+            "' of ", smb, "MB",
+            appendLF = FALSE
+        )
     }
 
     newname <- file.path (tempdir (), paste0 ("img", hash (10), ".png"))
@@ -131,11 +130,12 @@ reduce_size <- function (mapfile, quiet = TRUE)
     magick::image_resize (img, geometry = red) %>%
         magick::image_write (path = newname, comment = bbox)
 
-    if (!quiet)
-    {
+    if (!quiet) {
         snew <- formatC (file.size (newname) / 1e6, format = "f", digits = 1)
-        message ("\r", cli::symbol$tick, " Reduced size of '", mapfile,
-                 "' of ", smb, "MB to ", snew, "MB")
+        message (
+            "\r", cli::symbol$tick, " Reduced size of '", mapfile,
+            "' of ", smb, "MB to ", snew, "MB"
+        )
     }
 
     return (newname)
@@ -143,20 +143,21 @@ reduce_size <- function (mapfile, quiet = TRUE)
 # nocov end
 
 # maxdim is maximal pixel size in any one dimension
-reduce_image_size <- function (mapfile, maxdim = 1000, quiet = FALSE)
-{
+reduce_image_size <- function (mapfile, maxdim = 1000, quiet = FALSE) {
     o <- magick::image_read (mapfile)
     i <- magick::image_info (o)
     maxpix <- max (c (i$width, i$height))
     newname <- mapfile # default return value
-    if (maxpix > maxdim)
-    {
+    if (maxpix > maxdim) {
         # new name for modified file
         newname <- file.path (tempdir (), paste0 ("img", hash (10), ".png"))
         scl <- ceiling (maxpix / maxdim)
-        if (!quiet)
-            message (cli::symbol$tick, " Image [", mapfile,
-                     "] reduced in size by factor of ", scl)
+        if (!quiet) {
+            message (
+                cli::symbol$tick, " Image [", mapfile,
+                "] reduced in size by factor of ", scl
+            )
+        }
         dims <- paste0 (ceiling (c (i$width, i$height) / scl), collapse = "x")
         bbox <- magick::image_comment (o)
         magick::image_resize (o, geometry = dims) %>%
@@ -165,14 +166,13 @@ reduce_image_size <- function (mapfile, maxdim = 1000, quiet = FALSE)
     return (newname)
 }
 
-bbox_from_pdf <- function (file, as_string = FALSE)
-{
+bbox_from_pdf <- function (file, as_string = FALSE) {
     file <- paste0 (tools::file_path_sans_ext (file), ".pdf")
-    if (!file.exists (file))
+    if (!file.exists (file)) {
         stop ("file ", file, " does not exist")
-    bbox <- pdftools::pdf_info (file)$keys$Title    # nolint
-    if (!as_string)
-    {
+    }
+    bbox <- pdftools::pdf_info (file)$keys$Title # nolint
+    if (!as_string) {
         bbox <- strsplit (bbox, "\\+") [[1]]
         bbox [1] <- substring (bbox [1], 3, nchar (bbox [1])) # rm "EX"
         bbox <- as.numeric (bbox)
@@ -181,8 +181,7 @@ bbox_from_pdf <- function (file, as_string = FALSE)
 }
 # nocov end
 
-bbox_from_png <- function (file)
-{
+bbox_from_png <- function (file) {
     img <- magick::image_read (file)
     bbox <- magick::image_comment (img)
     bbox <- strsplit (bbox, "\\+") [[1]]
@@ -191,20 +190,14 @@ bbox_from_png <- function (file)
 }
 
 # trim white space from border of png images
-trim_white <- function (fname)
-{
+trim_white <- function (fname) {
     i <- magick::image_read (fname)
     bbox <- magick::image_comment (i)
     # change "EX" at start of file comment to "TX" to flag trimmed:
-    if (substring (bbox, 1, 1) != "T")
-    {
+    if (substring (bbox, 1, 1) != "T") {
         # nocov start
         # -- sample images have already been trimmed, so can't be tested
-        dims <- magick::image_info (i)
-        dims0 <- as.integer (dims [c ("width", "height")])
         img <- magick::image_trim (i, fuzz = 1)
-        dims <- magick::image_info (img)
-        dims1 <- as.integer (dims [c ("width", "height")])
         bbox <- paste0 ("T", substring (bbox, 2, nchar (bbox)))
         magick::image_write (img, path = fname, comment = bbox)
         # nocov end
--- inst/doc/mapscanner.R
+++ inst/doc/mapscanner.R
@@ -17,22 +17,24 @@ message ("Successfully generated 'chennai.pdf' and 'chennai.png'")
 ## ----chennaiBrick-fakey, eval = FALSE-----------------------------------------
 #  x <- ms_generate_map ("chennai india", mapname = "chennai")
 
-## ---- echo = FALSE------------------------------------------------------------
+## ----echo = FALSE-------------------------------------------------------------
 message ("Successfully generated 'chennai.pdf' and 'chennai.png'")
 
 ## ----x, eval = FALSE----------------------------------------------------------
 #  x
 
 ## ----chennaiBrick, echo = FALSE-----------------------------------------------
-x <- paste0 ("class      : RasterBrick\n",
-             "dimensions : 1147, 562, 644614, 3  (nrow, ncol, ncell, nlayers)\n",
-             "resolution : 38.21851, 38.21851  (x, y)\n",
-             "extent     : 8921157, 8942635, 1442787, 1486624  (xmin, xmax, ymin, ymax)\n",
-             "crs        : +proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs\n",
-             "source     : memory\n",
-             "names      : index_1.1, index_1.2, index_1.3\n",
-             "min values :       107,       107,       107\n",
-             "max values :       254,       254,       254")
+x <- paste0 (
+    "class      : RasterBrick\n",
+    "dimensions : 1147, 562, 644614, 3  (nrow, ncol, ncell, nlayers)\n",
+    "resolution : 38.21851, 38.21851  (x, y)\n",
+    "extent     : 8921157, 8942635, 1442787, 1486624  (xmin, xmax, ymin, ymax)\n",
+    "crs        : +proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs\n",
+    "source     : memory\n",
+    "names      : index_1.1, index_1.2, index_1.3\n",
+    "min values :       107,       107,       107\n",
+    "max values :       254,       254,       254"
+)
 message (x)
 
 ## ----chennaiBrick2, eval = FALSE----------------------------------------------
@@ -52,34 +54,36 @@ knitr::include_graphics (f)
 
 ## ----rectify-output, echo = FALSE---------------------------------------------
 m <- c (
-"Simple feature collection with 2 features and 0 fields",
-"Geometry type: POLYGON",
-"Dimension:     XY",
-"Bounding box:  xmin: -96.11801 ymin: 41.26638 xmax: -96.02722 ymax: 41.30108",
-"Geodetic CRS:  WGS 84",
-"                        geometry",
-"1 POLYGON ((-96.10692 41.2684...",
-"2 POLYGON ((-96.0276 41.2964,...")
+    "Simple feature collection with 2 features and 0 fields",
+    "Geometry type: POLYGON",
+    "Dimension:     XY",
+    "Bounding box:  xmin: -96.11801 ymin: 41.26638 xmax: -96.02722 ymax: 41.30108",
+    "Geodetic CRS:  WGS 84",
+    "                        geometry",
+    "1 POLYGON ((-96.10692 41.2684...",
+    "2 POLYGON ((-96.0276 41.2964,..."
+)
 m <- paste0 (m, collapse = "\n")
 message (m)
 
 ## ----mapshot-hulls, eval = FALSE, echo = FALSE--------------------------------
-#  xy$id <- seq (nrow (xy))
-#  if (!file.exists ("mapshot-polys.png"))
-#  {
+#  xy$id <- seq_len (nrow (xy))
+#  if (!file.exists ("mapshot-polys.png")) {
 #      x <- mapview::mapview (xy)
-#      mapview::mapshot (x, file = "mapshot-hulls.png",
-#                        remove_controls = c ("layersControl", "zoomControl"),
-#                        vwidth = vwidth, vheight = vheight)
+#      mapview::mapshot (x,
+#          file = "mapshot-hulls.png",
+#          remove_controls = c ("layersControl", "zoomControl"),
+#          vwidth = vwidth, vheight = vheight
+#      )
 #  }
 
 ## ----mapview-fakey, eval = FALSE----------------------------------------------
-#  xy$id <- seq (nrow (xy))
+#  xy$id <- seq_len (nrow (xy))
 #  mapview::mapview (xy)
 
 ## ----mapshot-hulls-png, echo = FALSE, out.width = "75%"-----------------------
 knitr::include_graphics ("./mapshot-hulls.png")
-#plot (xy)
+# plot (xy)
 
 ## ----type-polys, eval = FALSE-------------------------------------------------
 #  f_orig <- system.file ("extdata", "omaha.png", package = "mapscanner")
@@ -87,22 +91,23 @@ knitr::include_graphics ("./mapshot-hulls.png")
 #  xy <- ms_rectify_map (f_orig, f_mod, type = "polygons", quiet = TRUE)
 
 ## ----mapview-polys, eval = FALSE, echo = FALSE--------------------------------
-#  xy$id <- seq (nrow (xy))
-#  if (!file.exists ("mapshot-polys.png"))
-#  {
+#  xy$id <- seq_len (nrow (xy))
+#  if (!file.exists ("mapshot-polys.png")) {
 #      x <- mapview::mapview (xy)
-#      mapview::mapshot (x, file = "mapshot-polys.png",
-#                        remove_controls = c ("layersControl", "zoomControl"),
-#                        vwidth = vwidth, vheight = vheight)
+#      mapview::mapshot (x,
+#          file = "mapshot-polys.png",
+#          remove_controls = c ("layersControl", "zoomControl"),
+#          vwidth = vwidth, vheight = vheight
+#      )
 #  }
 
 ## ----mapview-polys-fakey, eval = FALSE----------------------------------------
-#  xy$id <- seq (nrow (xy))
+#  xy$id <- seq_len (nrow (xy))
 #  mapview::mapview (xy)
 
 ## ----mapshot-polys-png, echo = FALSE, out.width = "75%"-----------------------
 knitr::include_graphics ("./mapshot-polys.png")
-#plot (xy)
+# plot (xy)
 
 ## ----type-points, eval = FALSE------------------------------------------------
 #  f_orig <- system.file ("extdata", "omaha.png", package = "mapscanner")
@@ -110,31 +115,32 @@ knitr::include_graphics ("./mapshot-polys.png")
 #  xy <- ms_rectify_map (f_orig, f_mod, type = "points", quiet = TRUE)
 
 ## ----mapview-points, eval = FALSE, echo = FALSE-------------------------------
-#  xy$id <- seq (nrow (xy))
-#  if (!file.exists ("mapshot-points.png"))
-#  {
+#  xy$id <- seq_len (nrow (xy))
+#  if (!file.exists ("mapshot-points.png")) {
 #      x <- mapview::mapview (xy)
-#      mapview::mapshot (x, file = "mapshot-points.png",
-#                        remove_controls = c ("layersControl", "zoomControl"),
-#                        vwidth = vwidth, vheight = vheight)
+#      mapview::mapshot (x,
+#          file = "mapshot-points.png",
+#          remove_controls = c ("layersControl", "zoomControl"),
+#          vwidth = vwidth, vheight = vheight
+#      )
 #  }
 
 ## ----mapview-points-fakey, eval = FALSE---------------------------------------
-#  xy$id <- seq (nrow (xy))
+#  xy$id <- seq_len (nrow (xy))
 #  mapview::mapview (xy)
 
 ## ----mapshot-points-png, echo = FALSE, out.width = "75%"----------------------
 knitr::include_graphics ("./mapshot-points.png")
-#plot (xy)
+# plot (xy)
 
 ## ----random-polys-fakey, eval = FALSE-----------------------------------------
 #  n <- 5 # number of polygons
 #  polys <- lapply (seq (n), function (i) {
-#                       nxy <- 20 # number of points used to generate hull
-#                       xy <- matrix (runif (2 * nxy), ncol = 2)
-#                       h <- chull (xy)
-#                       sf::st_polygon (list (xy [c (h, h [1]), ]))
-#              })
+#      nxy <- 20 # number of points used to generate hull
+#      xy <- matrix (runif (2 * nxy), ncol = 2)
+#      h <- chull (xy)
+#      sf::st_polygon (list (xy [c (h, h [1]), ]))
+#  })
 #  polys <- sf::st_sf (n = seq (n), geometry = polys)
 
 ## ----random-polys-fakey2, eval = FALSE----------------------------------------
@@ -143,42 +149,56 @@ knitr::include_graphics ("./mapshot-points.png")
 #  aggr$type <- "aggregated polygons"
 #  polys <- rbind (polys, aggr)
 #  # Convert type to factor to order facets:
-#  polys$type <- factor (polys$type, levels = c ("raw polygons", "aggregated polygons"))
+#  polys$type <- factor (
+#      polys$type,
+#      levels = c ("raw polygons", "aggregated polygons")
+#  )
 #  library (ggplot2)
-#  ggplot (polys, aes (fill = n)) + geom_sf () + facet_wrap (~type)
+#  ggplot (polys, aes (fill = n)) +
+#      geom_sf () +
+#      facet_wrap (~type)
 
 ## ----random-polys, echo = FALSE, eval = TRUE----------------------------------
 set.seed (1)
 n <- 5 # number of polygons
 polys <- lapply (seq (n), function (i) {
-                     nxy <- 20 # number of points used to generate hull
-                     xy <- matrix (runif (2 * nxy), ncol = 2)
-                     h <- chull (xy)
-                     sf::st_polygon (list (xy [c (h, h [1]), ]))
-            })
+    nxy <- 20 # number of points used to generate hull
+    xy <- matrix (runif (2 * nxy), ncol = 2)
+    h <- chull (xy)
+    sf::st_polygon (list (xy [c (h, h [1]), ]))
+})
 polys <- sf::st_sf (n = seq (n), geometry = polys)
 aggr <- ms_aggregate_polys (polys)
 polys$type <- "raw polygons"
 aggr$type <- "aggregated polygons"
 polys <- rbind (polys, aggr)
-polys$type <- factor (polys$type, levels = c ("raw polygons", "aggregated polygons"))
+polys$type <- factor (
+    polys$type,
+    levels = c ("raw polygons", "aggregated polygons")
+)
 theme_set (theme_minimal ())
-ggplot (polys, aes (fill = n)) + geom_sf () + facet_wrap (~type)
+ggplot (polys, aes (fill = n)) +
+    geom_sf () +
+    facet_wrap (~type)
 
 ## ----aggr13, eval = TRUE------------------------------------------------------
-ggplot (aggr [aggr$n %in% c (1, 3, 5), ], aes (fill = n)) + geom_sf ()
+ggplot (aggr [aggr$n %in% c (1, 3, 5), ], aes (fill = n)) +
+    geom_sf ()
 
 ## ----aggr-area, eval = TRUE---------------------------------------------------
 aggr$area <- sf::st_area (aggr)
-ggplot (aggr, aes (x = n, y = area)) + geom_line (size = 2)
+ggplot (aggr, aes (x = n, y = area)) +
+    geom_line (size = 2)
 
 ## ----pdfinfo-fakey, eval = FALSE----------------------------------------------
 #  pdftools::pdf_info ("chennai.pdf")$keys
 
 ## ----pdfinfo, echo = FALSE----------------------------------------------------
-x <- list ("Title" = "EX8921118.44521949+1442748.9088827+8942673.68719591+1486623.76311839",
-           "Producer" = "R 4.0.0",
-           "Creator" = "R")
+x <- list (
+    "Title" = "EX8921118.44521949+1442748.9088827+8942673.68719591+1486623.76311839",
+    "Producer" = "R 4.0.0",
+    "Creator" = "R"
+)
 x
 
 ## ----img-comment-fakey, eval = FALSE------------------------------------------
